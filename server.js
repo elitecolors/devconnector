812c1d823dbe05fe7b332b6ba6ebe48504fec2b1
Ahmed <saidaniahmed125@gmail.com>
2024-05-28 13:09:41 +0200

    add monngoose config

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express');
-
+const connectDB = require('./config/db');
 const app = express();
 
+connectDB();
 app.get('/', (req, res) => res.send('API Running on localhost'));
 
 const PORT = process.env.PORT || 5000;

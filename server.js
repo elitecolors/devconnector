ea86e09d54347b4493cb8657134683364894ca8c
Ahmed <saidaniahmed125@gmail.com>
2024-05-28 13:19:34 +0200

    add router

--- server.js
+++ server.js
@@ -5,6 +5,11 @@ const app = express();
 connectDB();
 app.get('/', (req, res) => res.send('API Running on localhost'));
 
+app.use('/api/user', require('./routes/api/user'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));

aa925b615c10d69f49f80b86c5355606673d6c81
Ahmed <saidaniahmed125@gmail.com>
2024-05-28 14:42:36 +0200

    add models  * add user

--- routes/api/user.js
+++ routes/api/user.js
@@ -1,6 +1,25 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator');
 
-router.get('/', (req, res) => res.send('User route'));
+router.post('/',
+    [
+        check('name', 'name is required').not().isEmpty(),
+        check('email', 'email is required').isEmail(),
+        check('password','').isLength({min: 6, max: 8}),
+    ],
+    (req, res) =>
+{
+    const errors= validationResult(req);
+
+    if (! errors.isEmpty()) {
+        return res.status(400).json({
+            errors: errors.array(),
+        })
+    }
+    const email = req.body.email;
+    console.log(req.body);
+    res.send('user route')
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -3,6 +3,12 @@ const connectDB = require('./config/db');
 const app = express();
 
 connectDB();
+
+// init middleware
+
+app.use(express.json({
+    extended: false
+}));
 app.get('/', (req, res) => res.send('API Running on localhost'));
 
 app.use('/api/user', require('./routes/api/user'));
